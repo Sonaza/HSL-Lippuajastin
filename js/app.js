1c1c5a47bb111994aefb6b6ac79f79140a28f475
Sonaza <sonaza@iki.fi>
2023-02-20 15:47:55 +0200

    New ticket start time editor, added bus stops

--- js/app.js
+++ js/app.js
@@ -57,17 +57,111 @@ function parse_time_string(input_string)
 	if (isNaN(hour) || isNaN(minute))
 		return undefined;
 	
-	// Times given up to 110 minutes into the past are accepted but beyond that count as next day instead
-	const diff = (now.getTime() - new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute).getTime()) / 1000 / 60;
-	if (!(diff >= -110 && (hour > now.getHours() || (hour === now.getHours() && minute > now.getMinutes()))))
-	{
-		now.setDate(now.getDate() + 1);
-	}
-	return new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute);
+	return handle_futurepast(new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute));
 }
 
-app.controller('MainController', async ($rootScope, $scope, $timeout, $interval, $sce) =>
+function get_datetime(hours, minutes)
 {
+	const now = new Date();
+	return new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes);
+}
+
+// Wraps a date based on the time only so that relative to the given `now`
+// the date becomes yesterday, today or tomorrow when crossing midnight.
+function wrap_date(now, date_value)
+{
+	const hour = date_value.getHours();
+	const minute = date_value.getMinutes();
+	date_value = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute);
+	
+	const past   = new Date(date_value.getTime() - 24 * 3600 * 1000);
+	const future = new Date(date_value.getTime() + 24 * 3600 * 1000);
+	
+	const diff_present = Math.abs((now.getTime() - date_value.getTime()) / 1000 / 60);
+	const diff_past    = ((now.getTime() - past.getTime()) / 1000 / 60);
+	const diff_future  = ((future.getTime() - now.getTime()) / 1000 / 60);
+	
+	if (diff_present < diff_past && diff_present < diff_future)
+	{
+		return date_value;
+	}
+	
+	if (diff_past < diff_future)
+	{
+		return past;
+	}
+	else
+	{
+		return future;
+	}
+}
+
+function handle_futurepast(new_date)
+{
+	if (!new_date)
+		return undefined;
+	
+	const now = new Date();
+	
+	const hour = new_date.getHours();
+	const minute = new_date.getMinutes();
+	new_date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute);
+	
+	const date_wrapped = wrap_date(now, new_date);
+	
+	const max_ticket_duration = 110;
+	const ticket_deadline = new Date(now.getTime() - max_ticket_duration * 60 * 1000);
+	
+	if (date_wrapped > ticket_deadline)
+	{
+		return date_wrapped;
+	}
+	
+	return new_date;
+}
+
+class BusStop
+{
+	label;
+	url;
+	
+	constructor(label, url)
+	{
+		this.label = label;
+		this.url   = url;
+	}
+}
+
+app.directive('iframeSrc', function($parse, $timeout, $sce)
+{
+	return {
+		restrict: 'A',
+		scope: true,
+		link: function (scope, element, attrs)
+		{
+			scope.$parent.$watch(attrs.iframeSrc, function(condition)
+			{
+				const src_url = $parse(attrs.iframeSrc)(scope.$parent);
+				element[0].setAttribute('src', $sce.trustAsResourceUrl(src_url));
+			});
+		}
+	}
+});
+
+app.controller('MainController', async ($rootScope, $scope, $timeout, $interval, $filter, $sce, $window) =>
+{
+	$scope.body_classes = () =>
+	{
+		let output = [];
+		
+		if ($scope.bus_stops.visible)
+		{
+			output.push('unscrollable');
+		}
+		
+		return output.join(' ')
+	}
+	
 	$scope.tickets = [
 		new Ticket('AB',   80),
 		new Ticket('ABC',  90),
@@ -77,6 +171,34 @@ app.controller('MainController', async ($rootScope, $scope, $timeout, $interval,
 		new Ticket('CD',   90),
 		new Ticket('D',    80),
 	];
+	
+	$scope.bus_stops = {
+		visible  : false,
+		selected : 0,
+		list     : [
+			// Urls are for https://omatnaytot.hsl.fi
+			new BusStop('Lukutori',      '2dbbd932-960e-57d6-9763-8b581470ad1b'),
+			new BusStop('Piispanaukio',  'a1ccf80d-9de4-5a06-8240-8b524d3dd106'),
+			new BusStop('Matinkylä (M)', 'bad3a022-578c-55ea-818a-4970d39fd68e'),
+			new BusStop('Sello',         '9f5bdee5-ec3e-57c5-bdb8-8d6c77392a28'),
+		],
+		get_current_url : () =>
+		{
+			const current_stop = $scope.bus_stops.list[$scope.bus_stops.selected];
+			if (!current_stop)
+				return '';
+			
+			// return $sce.trustAsResourceUrl('https://omatnaytot.hsl.fi/static/?url=' + current_stop.url);
+			return 'https://omatnaytot.hsl.fi/static/?url=' + current_stop.url;
+		}
+	}
+	
+	$scope.bus_stops.stop_selected = (stop_index) =>
+	{
+		if ($scope.bus_stops.selected == stop_index)
+			return 'selected';
+		return '';
+	}
 	
 	$scope.find_ticket = (identifier) =>
 	{
@@ -110,21 +232,150 @@ app.controller('MainController', async ($rootScope, $scope, $timeout, $interval,
 		custom_start  : undefined,
 	}
 	
+	$scope.edit_ticket = {
+		opened        : false,
+		default_value : undefined,
+		custom_start  : undefined,
+		custom_start_value  : undefined,
+		dirty         : false,
+		input_field   : document.querySelector('#edit-dialog .current-value')
+	}
+	
+	$scope.edit_ticket.value_class = () =>
+	{
+		if (!$scope.edit_ticket.valid)
+			return 'invalid';
+		if ($scope.edit_ticket.dirty)
+			return 'dirty';
+		return '';
+	}
+	
+	let date_equals = (a, b) =>
+	{
+		if (a instanceof Date && b instanceof Date)
+		{
+			return Math.floor(a.getTime() / 60000) == Math.floor(b.getTime() / 60000);
+		}
+		return false;
+	}
+	
+	$scope.edit_ticket.open = () =>
+	{
+		$timeout(() =>
+		{
+			$scope.edit_ticket.opened = true;
+			
+			const now = new Date();
+			$scope.edit_ticket.default_value = ($scope.current_ticket.custom_start || now);
+			
+			$scope.edit_ticket.custom_start = undefined;
+			$scope.edit_ticket.valid = true;
+			
+			$timeout(() =>
+			{
+				$scope.edit_ticket.custom_start = $scope.edit_ticket.default_value;
+				$scope.edit_ticket.valid = true;
+				
+				$timeout(() =>
+				{
+					document.querySelector('#edit-dialog .current-value').focus();
+					// document.querySelector('#edit-dialog .current-value').click();
+					$scope.edit_ticket.dirty = false;
+				});
+			
+			});
+		})
+	}
+	
+	$scope.$watch('edit_ticket.custom_start', function(new_value, old_value)
+	{
+		if (!$scope.edit_ticket.opened)
+			return;
+		
+		// $scope.edit_ticket.custom_start_value = $filter('date')(new_value, 'HH:mm');
+		
+		$scope.edit_ticket.valid = new_value instanceof Date;
+		$scope.edit_ticket.dirty = !date_equals(new_value, $scope.edit_ticket.default_value);
+	}, true);
+	
+	$scope.edit_ticket.increment = (diff) =>
+	{
+		const parsed_time = $scope.edit_ticket.custom_start || new Date();
+		const new_time = new Date(parsed_time.getTime() + diff * 60 * 1000);
+		
+		$scope.edit_ticket.custom_start = new_time;
+	}
+	
+	$scope.handle_futurepast = handle_futurepast;
+	
+	
+	$scope.edit_ticket.reset = () =>
+	{
+		$timeout(() =>
+		{
+			$scope.edit_ticket.custom_start = $scope.edit_ticket.default_value;
+			
+			$timeout(() =>
+			{
+				$scope.edit_ticket.dirty = false;
+			});
+		});
+	}
+	
+	$scope.edit_ticket.cancel = () =>
+	{
+		$timeout(() =>
+		{
+			$scope.edit_ticket.opened = false;
+		});
+	}
+	
+	$scope.edit_ticket.submit = () =>
+	{
+		// console.log("valid", $scope.edit_ticket.valid);
+		// console.log("dirty", $scope.edit_ticket.dirty);
+		
+		const date_handled = handle_futurepast($scope.edit_ticket.custom_start);
+		// console.log(date_handled);
+		if ($scope.edit_ticket.valid && $scope.edit_ticket.dirty)
+		{
+			$scope.current_ticket.custom_start = date_handled;
+		}
+		else
+		{
+			$scope.current_ticket.custom_start = undefined;
+		}
+		
+		$scope.edit_ticket.opened = false;
+	}
+	
+	angular.element($window).on('click', ($event) =>
+	{
+		if (!$scope.edit_ticket.opened)
+			return;
+		
+		if ($event.target.closest('#edit-dialog .inner'))
+			return;
+		
+		$scope.edit_ticket.cancel();
+		$event.preventDefault();
+	});
+	
+	angular.element($window).on('keydown', ($event) =>
+	{
+		if ($event.repeat || $event.ctrlKey || $event.altKey || $event.metaKey) return;
+		
+		if ($event.keyCode == 27) // ESC-key
+		{
+			$scope.edit_ticket.cancel();
+			$event.preventDefault();
+			return;
+		}
+	});
+	
 	$scope.edit_start = () =>
 	{
-		const default_value = ($scope.current_ticket.custom_start || new Date()).toLocaleTimeString("fi-FI", {
-			timeStyle: 'short',
-		});
-		
-		const custom_start = prompt("Valitse lipun aloitusajankohta (käytä oletusta jättämällä tyhjäksi).\n\nSyötä aika muodossa HH.MM tai vaihtoehtoisesti erotus minuuteissa:", default_value);
-		if (custom_start === null)
-			return;
-		
-		// Keep default if nothing was changed and nothing was yet changed either
-		if ($scope.current_ticket.custom_start === undefined && custom_start == default_value)
-			return;
-		
-		$scope.current_ticket.custom_start = parse_time_string(custom_start);
+		$scope.edit_ticket.open();
 	}
 	
 	$scope.current_custom_start = () =>
